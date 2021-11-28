6d7f683482227869baf838180d8427257a5c204a
mmelendez2 <melendez5113@gmail.com>
2021-11-28 15:48:04 -0500

    Analyzing retiree data using SQL

---
+++ Employee_Challenge.sql
@@ -0,0 +1,53 @@
+
+-- List of retirees with titles
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+JOIN titles As t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+-- List of unique retirees wih titles
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO unique_titles
+FROM employees AS e
+JOIN titles As t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, t.to_date DESC;
+
+-- List of retirees by most recent title
+SELECT COUNT (ut.title), ut.title
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY COUNT (ut.title) DESC;
+
+-- List of mentorship eligibility
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
